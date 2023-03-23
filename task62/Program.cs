cb2d510987811762b47b55316c3f6bec8b545884
halfin rodion <halfin87@mail.ru>
2023-03-23 15:03:46 +0300

    task62

---
+++ task62/Program.cs
@@ -0,0 +1,85 @@
+﻿// Заполните спирально массив 4 на 4.
+
+// Например, на выходе получается вот такой массив:
+
+// 1 2 3 4
+
+// 12 13 14 5
+
+// 11 16 15 6
+
+// 10 9 8 7
+
+// int [,] GetSpiralArray (int rows, int columns)
+// {
+//   int[,] array = new int [rows, columns];
+//   int count = 1;
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//    for (int j = 0; j < array.GetLength(1); j++)
+//    {
+//     array[i,j] = count;
+//     count++;
+//     while (j = array.GetLength(1))
+//     count++;
+//     {
+//       while (i = array.GetLength(0))
+//       count++;
+//     }
+//    } 
+//   }
+//   return array;
+// }
+
+// void PrintArray(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       Console.Write(array[i, j] + " ");
+//     }
+//     Console.WriteLine();
+//   }
+// }
+
+// int[,] myarray = GetSpiralArray(4, 4);
+// PrintArray(myarray);
+
+int n = 4;
+int[,] sqareMatrix = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+{
+  sqareMatrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+WriteArray(sqareMatrix);
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
